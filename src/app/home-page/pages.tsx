4838000093360be003102a9714dabec2b6537239
Kartikeya167 <147020097+Kartikeya-16@users.noreply.github.com>
2025-02-28 10:32:10 +0530

    Revert "home faq section"
    This reverts commit b086bd3f7f2dab293a56369952ba890aa9ed5004.

--- src/app/home-page/pages.tsx
+++ src/app/home-page/pages.tsx
@@ -1,24 +1,21 @@
 import React from 'react'
-// import Navbar from './components/Navbar'
+import Navbar from './components/Navbar'
 import Hero from "./components/Hero"
 import Services from './components/Services'
 import About from './components/About';
-// import Gallery from './components/Gallery';
+import Gallery from './components/Gallery';
 import Blog from "./components/Blog"
 import ContactUS from "./components/ContactUs"
-import Faq from './components/faq';
 
 const pages = () => {
   return (
     <div>
       <Hero />
-      {/* <Gallery /> */}
+      <Gallery />
       <Services />
       <Blog />
       <ContactUS />
       <About />
-      <Faq/>
-
     </div>
   )
 }
